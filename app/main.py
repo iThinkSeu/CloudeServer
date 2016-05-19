ae091c99efd9cad9fe424d352a77a27525b4a313
iThink <yqsseu@163.com>
2016-05-19 20:11:48 +0800

    static flask first sucessful

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
+#-*- coding: UTF-8 -*- 
 from flask import Flask
 from flask import request
 from flask import render_template
-from flask import redirect
+from flask import redirect,url_for
 from models import *
 from wtforms import Form,TextField,PasswordField,validators
 
@@ -32,6 +33,12 @@ def login():
 			return "Login Failed"
 	return render_template('index.html',form=myForm)
 
+@app.route("/",methods=['GET','POST'])
+def index():
+	print "hi"
+	#return "hello"
+	#return redirect(url_for('static', filename='profile_small.jpg'), code=301)
+	return render_template('managebase.html')
 
 if __name__ == '__main__':
-	app.run(port=5001)
+	app.run(port=3001,debug=True)
