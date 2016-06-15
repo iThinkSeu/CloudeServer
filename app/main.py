30eeac891b345485d4ff474bc4aa3d938f8628b6
iThink <yqsseu@163.com>
2016-06-15 17:05:07 +0800

    433 test

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 #-*- coding: UTF-8 -*- 
 from flask import Flask
-from flask import request
+from flask import request,jsonify,json
 from flask import render_template
 from flask import redirect,url_for
 from models import *
 from wtforms import Form,TextField,PasswordField,validators
+from hashmd5 import *
+import string
 
 app = Flask(__name__)
 
@@ -17,9 +19,35 @@ class LoginForm(Form):
 def register():
 	myForm = LoginForm(request.form)
 	if request.method=='POST':
-		u=User(myForm.username.data,myForm.password.data)
-		u.add()
-		return "register sucessful"
+		username = myForm.username.data
+		password = myForm.password.data
+		token = ''
+		id = ''
+		temp = checkName(username)
+		if temp==False:		
+			response = jsonify({
+								'id':'',
+								'state':'fail',
+								'reason':'用户名不能包含中文且至少要两个字母',
+								'token':'chinese'})
+			return response
+		token= hashToken(username,password)
+		u=User(username=username,password=password,token=token)
+		if u.isExistedusername() == 0:
+			##未完成，加code验证码判断相关逻辑
+			u.add()
+			state = 'successful'
+			reason = ''
+			token = hashToken(username,password)
+			id = getuserinformation(token).id
+		else:
+			state = 'fail'
+			reason = '用户名已被注册'
+			token = 'Haveresiger'
+			id=''
+		response = jsonify({'state':state,'reason':reason,'token':token,'id':id})
+		return response
+		#return "register sucessful"
 	return render_template('index.html',form = myForm)
 
 @app.route("/login",methods=['GET','POST'])
@@ -39,6 +67,75 @@ def index():
 	#return "hello"
 	#return redirect(url_for('static', filename='profile_small.jpg'), code=301)
 	return render_template('managebase.html',userinfo = "zrr")
+
+
+@app.route("/postmeasuredata",methods=['GET','POST'])
+def postmeasuredata():
+	try:
+		token = request.json['token']
+		datatype = request.json.get('datatype','')
+		value_string = request.json.get('value','')
+		value = float(str(value_string))
+		u = getuserinformation(token)
+		if u is not None:
+			if(datatype in ['VDC','VAC','IDC','IAC']):
+				tmpmeasure = Measuredata(datatype = datatype,value = value)
+				u.publishmeasuredata(tmpmeasure)
+				state = 'successful'
+				reason = ''
+			else:
+				state = 'fail'
+				reason = 'no this measure type'				
+		else:
+			state = 'fail'
+			reason = 'no user'
+	except Exception, e:	
+		print e
+		state = 'fail'
+		reason = 'exception'
+
+	response = jsonify({'state':state,
+						'reason':reason})
+	return response
+
+@app.route("/appregister",methods=['POST'])
+def appregister():
+	try:
+		username = request.json.get('username','')
+		password = request.json.get('password','')
+		token = ''
+		id = ''
+		temp = checkName(username)
+		if temp==False:		
+			response = jsonify({
+								'id':'',
+								'state':'fail',
+								'reason':'用户名不能包含中文且至少要两个字母',
+								'token':'chinese'})
+			return response
+		token= hashToken(username,password)
+		u=User(username=username,password=password,token=token)
+		if u.isExistedusername() == 0:
+			##未完成，加code验证码判断相关逻辑
+			u.add()
+			state = 'successful'
+			reason = ''
+			token = hashToken(username,password)
+			id = getuserinformation(token).id
+		else:
+			state = 'fail'
+			reason = '用户名已被注册'
+			token = 'Haveresiger'
+			id=''
+	
+	except Exception, e:	
+		print e
+		state = 'fail'
+		reason = 'exception'
+
+	response = jsonify({'state':state,'reason':reason,'token':token,'id':id})
+	return response
+
 
 if __name__ == '__main__':
 	app.run(port=3000,debug=True)
--- app/models.py
+++ app/models.py
@@ -3,11 +3,12 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
+from datetime import *
 #from flask.ext.sqlalchemy import SQLALchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']="mysql://root:root@localhost:3306/dataserverble?charset=utf8"
+app.config['SQLALCHEMY_DATABASE_URI']="mysql://root:0596@223.3.56.220:3306/dataserverble?charset=utf8"
 
 db = SQLAlchemy(app)
 migrage = Migrate(app,db)
@@ -15,31 +16,74 @@ manager = Manager(app)
 manager.add_command('db',MigrateCommand)
 
 class User(db.Model):
+	__tablename__ = "users"
 	id = db.Column(db.Integer,primary_key=True)
 	username = db.Column(db.String(32),unique = True)
 	password = db.Column(db.String(32))
-	def __init__(self,username,password):
-		self.username = username
-		self.password = password
+	token = db.Column(db.String(32))
+	measuredatas = db.relationship('Measuredata',backref = 'instrument', lazy = 'dynamic')
+
 	def add(self):
 		try:
-			db.session.add(self)
-			db.session.commit()
-			return self.id_
+			tempuser = User.query.filter_by(username=self.username).first()
+			if tempuser is None:
+				db.session.add(self)
+				db.session.commit()
+				return 0
+			else:
+				return 1
 		except Exception, e:
+			print e
 			db.session.rollback()
-			return e
-		finally:
-			return 0
-
+			return 2
 	def isExisted(self):
 		tempuser = User.query.filter_by(username=self.username,password=self.password).first()
 		if tempuser is None:
 			return 0
 		else:
 			return 1
+	def isExistedusername(self):
+		tempuser = User.query.filter_by(username = self.username).first()
+		if tempuser is None:
+			return 0
+		else:
+			return 1
+	def publishmeasuredata(self,measuredata):
+		try:
+			measuredata.instrument = self
+			db.session.add(measuredata)
+			db.session.execute('set names utf8mb4')
+			db.session.commit()
+			return 0
+		except Exception, e:
+			print e
+			db.session.rollback()
+			return 2	
 
+class Measuredata(db.Model):
+	__tablename__ = "messuredatas"
+	id = db.Column(db.Integer,primary_key = True)
+	instrumentid = db.Column(db.Integer,db.ForeignKey('users.id'))
+	datatype = db.Column(db.String(32))
+	value = db.Column(db.Float)
+	timestamp = db.Column(db.DateTime, default = datetime.now)
+	def add(self):
+		try:
+			db.session.add(self)
+			db.session.execute('set names utf8mb4')
+			db.session.commit()
+		except Exception, e:
+			print e
+			db.session.rollback()
+			return 2
 
+def getuserinformation(token):
+	u=User.query.filter_by(token=token).first()
+	return u 
+
+def getTokeninformation(username):
+	u=User.query.filter_by(username=username).first()
+	return u 
 
 if __name__ == '__main__':
 	manager.run()
